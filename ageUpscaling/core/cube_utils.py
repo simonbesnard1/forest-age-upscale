96a4a98f4ea8c7356666d455c584fb87c20c6c8f
Simon <besnard@gfz-potsdam.de>
2022-12-06 15:47:45 +0100

    updated gube modules

--- ageUpscaling/core/cube_utils.py
+++ ageUpscaling/core/cube_utils.py
@@ -5,99 +5,171 @@ Created on Thu Dec  1 12:36:53 2022
 
 @author: simon
 """
-from typing import Tuple, Sequence, Callable, Union
-import inspect
+from typing import Union
+from abc import ABC
 import os
+import atexit
+
+from ageUpscaling.utils.utilities import async_run
 
 import numpy as np
 import xarray as xr
+import dask
 
-from xcube.core.schema import CubeSchema
-from xcube.core.chunkstore import ChunkStore
+import zarr
+import shutil
+synchronizer = zarr.ProcessSynchronizer('.zarrsync')
 
-CubeFuncOutput = Union[xr.DataArray, np.ndarray, Sequence[Union[xr.DataArray, np.ndarray]]]
-CubeFunc = Callable[..., CubeFuncOutput]
+def cleanup():
+    if os.path.isdir('.zarrsync') and (len(os.listdir('.zarrsync')) == 0):
+        shutil.rmtree('.zarrsync')
+atexit.register(cleanup)
 
-_PREDEFINED_KEYWORDS = ['input_params', 'dim_coords', 'dim_ranges']
-
-def _inspect_cube_func(cube_func: CubeFunc, input_var_names: Sequence[str] = None):
-    args, varargs, varkw, defaults, kwonlyargs, kwonlydefaults, annotations \
-        = inspect.getfullargspec(cube_func)
-    cube_func_name = '?'
-    if hasattr(cube_func, '__name__'):
-        cube_func_name = cube_func.__name__
-    true_args = [arg not in _PREDEFINED_KEYWORDS for arg in args]
-    if False in true_args and any(true_args[true_args.index(False):]):
-        raise ValueError(f'invalid cube_func {cube_func_name!r}: '
-                         f'any argument must occur before any of {", ".join(_PREDEFINED_KEYWORDS)}, '
-                         f'but got {", ".join(args)}')
-    if not all(true_args) and varargs:
-        raise ValueError(f'invalid cube_func {cube_func_name!r}: '
-                         f'any argument must occur before any of {", ".join(_PREDEFINED_KEYWORDS)}, '
-                         f'but got {", ".join(args)} before *{varargs}')
-    num_input_vars = len(input_var_names) if input_var_names else 0
-    num_args = sum(true_args)
-    if varargs is None and num_input_vars != num_args:
-        raise ValueError(f'invalid cube_func {cube_func_name!r}: '
-                         f'expected {num_input_vars} arguments, '
-                         f'but got {", ".join(args)}')
-    has_input_params = 'input_params' in args or 'input_params' in kwonlyargs
-    has_dim_coords = 'dim_coords' in args or 'dim_coords' in kwonlyargs
-    has_dim_ranges = 'dim_ranges' in args or 'dim_ranges' in kwonlyargs
-    return has_input_params, has_dim_coords, has_dim_ranges
-
-
-def _gen_index_var(cube_schema: CubeSchema):
-    dims = cube_schema.dims
-    shape = cube_schema.shape
-    chunks = cube_schema.chunks
-
-    # noinspection PyUnusedLocal
-    def get_chunk(cube_store: ChunkStore, name: str, index: Tuple[int, ...]) -> bytes:
-        data = np.zeros(cube_store.chunks, dtype=np.uint64)
-        data_view = data.ravel()
-        if data_view.base is not data:
-            raise ValueError('view expected')
-        if data_view.size < cube_store.ndim * 2:
-            raise ValueError('size too small')
-        for i in range(cube_store.ndim):
-            j1 = cube_store.chunks[i] * index[i]
-            j2 = j1 + cube_store.chunks[i]
-            data_view[2 * i] = j1
-            data_view[2 * i + 1] = j2
-        return data.tobytes()
-
-    store = ChunkStore(dims, shape, chunks)
-    store.add_lazy_array('__index_var__', '<u8', get_chunk=get_chunk)
-
-    dataset = xr.open_zarr(store)
-    index_var = dataset.__index_var__
-    index_var = index_var.assign_coords(**cube_schema.coords)
-    return index_var
-
-def new_cube(name:str='test_cube',
-             cube_location:str='/home/simon/Documents/science/GFZ/projects/forest-age-upscale/output',
-             cube_config:dict= {}):
+class ComputeCube(ABC):
     """
-    Create a new empty cube. Useful for creating cubes templates with
-    predefined coordinate variables and metadata.
-    :return: A cube instance
+    A schema that can be used to create new xcube datasets.
+    The given *shape*, *dims*, and *chunks*, *coords* apply to all data variables.
+
+    :param shape: A tuple of dimension sizes.
+    :param coords: A dictionary of coordinate variables. Must have values for all *dims*.
+    :param dims: A sequence of dimension names. Defaults to ``('time', 'lat', 'lon')``.
+    :param chunks: A tuple of chunk sizes in each dimension.
     """
-        
-    coords = cube_config['output_writer_params']['coords']
-    _ds = []
-    for dim in coords.keys():
-        if dim == 'latitude':
-            dims_ = np.arange(coords[dim][0], coords[dim][1], cube_config['spatial_resolution']) * -1 
-        elif dim == 'longitude':    
-            dims_ = np.arange(coords[dim][0], coords[dim][1], cube_config['spatial_resolution'])
-        elif dim == 'time':    
-            dims_ = np.arange(np.datetime64(coords[dim][0]), np.datetime64(coords[dim][1]), np.timedelta64(1, cube_config['temporal_resolution']))
-        
-        _ds.append( xr.Dataset(data_vars = {}, coords={dim:dims_}))
 
-    _ds = xr.merge(_ds).chunk(cube_config['output_writer_params']['chunksizes'])
+    def __init__(self,
+                 cube_config:dict= None):
+        
+        self.dims_= cube_config['output_writer_params']['dims']
+        self.variables = cube_config['output_variables']
+        self.cube_location = cube_config['cube_location']
+        self.chunksizes = cube_config['output_writer_params']['chunksizes']
+        self.temporal_resolution = cube_config['temporal_resolution']
+        self.spatial_resolution = cube_config['spatial_resolution']
+        
+    def init_variable(self, dataset, njobs=None, parallel=True):
+        """init_variable(dataset)
 
-    _ds.to_zarr(os.path.join(cube_location, name), consolidated=True)
+        Initializes all dataset variables in the Cube.
+
+        Parameters
+        ----------
+        dataset : xr.Dataset, xr.DataArray, dictionary, or tuple
+            Must be either xr.Dataset or xr.DataArray objects,
+            or a dictionary with of the form {var_name: dims}
+            where var_name is a string and dims is a list of
+            dimension names as stings.
+
+        njobs : int, default is None
+            Number of cores to use in parallel when writing data, None will
+            result in the default njobs as defined in during initialization.
+
+        """
+        #TODO
+        
+        if njobs is None:
+            njobs = self.njobs
+        self._init_cube()
+        if type(dataset) is xr.DataArray:
+            self._init_zarr_variable((dataset.name, dataset.dims, dataset.attrs, dataset.dtype))
+        elif type(dataset) is tuple:
+            self._init_zarr_variable(dataset)
+        else:
+            to_proc = []
+            if type(dataset) is xr.Dataset:
+                for _var in set(dataset.variables) - set(dataset.coords):
+                    to_proc.append((_var, dataset[_var].dims, dataset[_var].attrs, dataset[_var].dtype))
+            elif type(dataset) is dict:
+                for k, v in dataset.items():
+                    if type(v) is str:
+                        to_proc.append((k, v, None, np.float64))
+                    elif type(v) is dict:
+                        to_proc.append((k, v['dims'], v['attrs'], np.float64))
+                    elif len(v) == 2:
+                        to_proc.append((k, v[0], v[1], np.float64))
+                    else:
+                        raise ValueError(
+                            "key:value pair must be constructed as one of: var_name:(dims, attrs),\
+                                var_name:{dims:dims, attrs:attrs}, or var_name:dim")
+            else:
+                raise RuntimeError("dataset must be xr.Dataset, xr.DataArray, dictionary, or tuple")
+            _ = async_run(self._init_zarr_variable, to_proc, njobs)
+        self.cube = xr.open_zarr(self.cube_location)
     
-    
+    def new_cube(self) -> xr.Dataset:
+        """
+        Create a new empty cube. Useful for creating cubes templates with
+        predefined coordinate variables and metadata.
+        :return: A cube instance
+        """
+            
+        ds_ = []
+        coords = {}
+        for dim in self.dims_.keys():
+            if dim == 'latitude':
+                dim_ = np.arange(self.dims_[dim][0], self.dims_[dim][1], self.spatial_resolution) * -1 
+            elif dim == 'longitude':    
+                dim_ = np.arange(self.dims_[dim][0], self.dims_[dim][1], self.spatial_resolution)
+            elif dim == 'time':    
+                dim_ = np.arange(np.datetime64(self.dims_[dim][0]), np.datetime64(self.dims_[dim][1]),
+                                  np.timedelta64(1, self.temporal_resolution))
+            coords.update({dim: dim_})
+        
+        dims = coords.keys()
+        shape = [v.size for v in coords.values() if len(v.shape) > 0]
+        data_vars = {}   
+        
+        if self.variables:
+            for var_name in self.variables:
+                
+                data_vars[var_name] = xr.DataArray(dask.array.full(shape=shape,
+                                                                    chunks=self.chunksizes, 
+                                                                    fill_value=np.nan),
+                                                    coords=coords,
+                                                    dims=dims,
+                                                    name=var_name
+                                                ).chunk(self.chunksizes)
+                
+            ds_ = xr.Dataset(data_vars=data_vars, coords=coords)
+        
+        else :
+            ds_ = xr.Dataset(data_vars={}, coords=coords)        
+            
+        ds_.to_zarr(self.cube_location, consolidated=True)
+        
+    def _update_cube_DataArray(self, 
+                               da: Union[xr.DataArray, xr.Dataset]):
+        """
+        Updates a single DataArray in the zarr cube. Data must be pre-sorted.
+        Inputs to the `update_cube` function ultimately are passed here.
+        """
+        
+        try:
+            _zarr = zarr.open_group(self.cube_location, synchronizer = synchronizer)[da.name]
+        except ValueError as e:
+            raise FileExistsError("cube_location already exists but is not a zarr group. Delete existing directory or choose a different cube_location: "+self.cube_location) from e
+        
+        idxs = tuple([np.where( np.isin(self.cube[dim].values, da[dim].values ) )[0] for dim in da.dims])
+
+        if len(_zarr.shape) != len(da.shape):
+            raise ValueError("Inconsistent dimensions. Array `{0}` to be saved has dimensions of {1}, but target dataset expected {2}.".format(da.name, da.dims, self.cube[da.name].dims))
+        try:
+            _zarr.set_orthogonal_selection(idxs, da.data)
+        except Exception as e:
+            raise RuntimeError("Failed to write variable to cube: "+str(da)) from e
+        
+    def _update(self, 
+                da:Union[xr.DataArray, xr.Dataset], 
+                njobs:int=None):
+        """
+        Handles both Datasets and DataArrays.
+        """
+        if njobs is None:
+            njobs = self.njobs
+        update_function = self._update_cube_DataArray
+        if type(da) is xr.Dataset:
+            to_proc = [da[_var] for _var in (set(da.variables) - set(da.coords))]
+            _ = async_run(update_function, to_proc, njobs)
+        elif type(da) is xr.DataArray:
+            update_function(da)
+        else:
+            raise RuntimeError("Input must be xr.Dataset or xr.DataArray objects")
--- ageUpscaling/core/test_cube.py
+++ ageUpscaling/core/test_cube.py
@@ -5,35 +5,15 @@ Created on Thu Dec  1 12:10:34 2022
 
 @author: simon
 """
-from typing import Sequence, Dict, Any, Callable, Union, AbstractSet
-from abc import ABC
+from typing import Union
 import os
-import warnings
-import atexit
 import yaml as yml
 
-import numpy as np
 import xarray as xr
 
-from xcube.core.schema import CubeSchema
+from ageUpscaling.core.cube_utils import ComputeCube
 
-from ageUpscaling.core.cube_utils import _inspect_cube_func, _gen_index_var, new_cube
-
-CubeFuncOutput = Union[xr.DataArray, np.ndarray, Sequence[Union[xr.DataArray, np.ndarray]]]
-CubeFunc = Callable[..., CubeFuncOutput]
-
-_PREDEFINED_KEYWORDS = ['input_params', 'dim_coords', 'dim_ranges']
-
-import zarr
-import shutil
-synchronizer = zarr.ProcessSynchronizer('.zarrsync')
-
-def cleanup():
-    if os.path.isdir('.zarrsync') and (len(os.listdir('.zarrsync')) == 0):
-        shutil.rmtree('.zarrsync')
-atexit.register(cleanup)
-
-class DataCube(ABC):
+class DataCube(ComputeCube):
     """DataCube(cube_location, coords=None, chunks = None, njobs=1)
 
     Handles creation and updating of regularized cube zarr files.
@@ -55,205 +35,54 @@ class DataCube(ABC):
         Number of cores to use in parallel when writing data.
 
     """
-    
-    def load_cube(self):
+    def _init_cube(self):
         """
         Reloads the zarr file. If one does not yet exists, an empty one will be created.
         """
-        if os.path.isdir(os.path.join(self.cube_location + self.cube_name))==False:
-            new_cube(self.cube_name, self.cube_location, self.CubeConfig)
-
-        self.cube = xr.open_zarr(self.cube_location + self.cube_name , synchronizer= synchronizer)
+        if not os.path.isdir(self.cube_config['cube_location']):
+            self.new_cube()
+        
+        self.cube = xr.open_zarr(self.cube_config['cube_location'])
     
     def __init__(self, 
-                 cube_name:str = 'test_cube',
-                 cube_location:str= '/home/simon/Documents/science/GFZ/projects/forest-age-upscale/output/', 
                  cube_config_path:str='/home/simon/Documents/science/GFZ/projects/forest-age-upscale/experiments/config_global_cube.yaml'):
-        self.cube_location = cube_location
-        self.cube_name = cube_name
         
         with open(cube_config_path, 'r') as f:
-            self.CubeConfig =  yml.safe_load(f)
+            self.cube_config =  yml.safe_load(f)
+            
+        super().__init__(self.cube_config)
+        
+        self._init_cube()
+        
+    def compute_cube(self, 
+                     da: Union[xr.DataArray, xr.Dataset],
+                     njobs:int =1,
+                     initialize:bool=True):
+        """update_cube(da, njobs=None, initialize=True)
 
-        self.cube = None
-        self.load_cube()
-    
-        def compute_dataset(self,
-                            cube_func: CubeFunc,
-                            *input_cubes: xr.Dataset,
-                            input_cube_schema: CubeSchema = None,
-                            input_var_names: Sequence[str] = None,
-                            input_params: Dict[str, Any] = None,
-                            output_var_name: str = 'output',
-                            output_var_dims: AbstractSet[str] = None,
-                            output_var_dtype: Any = np.float64,
-                            output_var_attrs: Dict[str, Any] = None,
-                            vectorize: bool = None,
-                            cube_asserted: bool = False) -> xr.Dataset:
-            """
-            Compute a new output dataset with a single variable named *output_var_name*
-            from variables named *input_var_names* contained in zero, one, or more
-            input data cubes in *input_cubes* using a cube factory function *cube_func*.
-            *cube_func* is called concurrently for each of the chunks of the input variables.
-            It is expected to return a chunk block whith is type ``np.ndarray``.
-            If *input_cubes* is not empty, *cube_func* receives variables as specified by *input_var_names*.
-            If *input_cubes* is empty, *input_var_names* must be empty too, and *input_cube_schema*
-            must be given, so that a new cube can be created.
-            The full signature of *cube_func* is:::
-                def cube_func(*input_vars: np.ndarray,
-                              input_params: Dict[str, Any] = None,
-                              dim_coords: Dict[str, np.ndarray] = None,
-                              dim_ranges: Dict[str, Tuple[int, int]] = None) -> np.ndarray:
-                    pass
-            The arguments are:
-            * ``input_vars``: the variables according to the given *input_var_names*;
-            * ``input_params``: is this call's *input_params*, a mapping from parameter name to value;
-            * ``dim_coords``: a mapping from dimension names to the current chunk's coordinate arrays;
-            * ``dim_ranges``: a mapping from dimension names to the current chunk's index ranges.
-            Only the ``input_vars`` argument is mandatory. The keyword arguments
-            ``input_params``, ``input_params``, ``input_params`` do need to be present at all.
-            *output_var_dims* my be given in the case, where ...
-            TODO: describe new output_var_dims...
-            :param cube_func: The cube factory function.
-            :param input_cubes: An optional sequence of input cube datasets, must be provided if *input_cube_schema* is not.
-            :param input_cube_schema: An optional input cube schema, must be provided if *input_cubes* is not.
-            :param input_var_names: A sequence of variable names
-            :param input_params: Optional dictionary with processing parameters passed to *cube_func*.
-            :param output_var_name: Optional name of the output variable, defaults to ``'output'``.
-            :param output_var_dims: Optional set of names of the output dimensions,
-                used in the case *cube_func* reduces dimensions.
-            :param output_var_dtype: Optional numpy datatype of the output variable, defaults to ``'float32'``.
-            :param output_var_attrs: Optional metadata attributes for the output variable.
-            :param vectorize: Whether all *input_cubes* have the same variables which are concatenated and passed as vectors
-                to *cube_func*. Not implemented yet.
-            :param cube_asserted: If False, *cube* will be verified, otherwise it is expected to be a valid cube.
-            :return: A new dataset that contains the computed output variable.
-            """
-            if vectorize is not None:
-                # TODO: support vectorize = all cubes have same variables and cube_func
-                #       receives variables as vectors (with extra dim)
-                raise NotImplementedError('vectorize is not supported yet')
-                    
-            # Check compatibility of inputs
-            if input_cubes:
-                input_cube_schema = CubeSchema.new(input_cubes[0])
-                for cube in input_cubes:
-                    if cube != input_cubes[0]:
-                        # noinspection PyUnusedLocal
-                        other_schema = CubeSchema.new(cube)
-                        # TODO (forman): broadcast all cubes to same shape, rechunk to same chunks
-            elif input_cube_schema is None:
-                raise ValueError('input_cube_schema must be given')
-        
-            output_var_name = output_var_name or 'output'
-        
-            # Collect named input variables, raise if not found
-            input_var_names = input_var_names or []
-            input_vars = []
-            for var_name in input_var_names:
-                input_var = None
-                for cube in input_cubes:
-                    if var_name in cube.data_vars:
-                        input_var = cube[var_name]
-                        break
-                if input_var is None:
-                    raise ValueError(f'variable {var_name!r} not found in any of cubes')
-                input_vars.append(input_var)
-        
-            # Find out, if cube_func uses any of _PREDEFINED_KEYWORDS
-            has_input_params, has_dim_coords, has_dim_ranges = _inspect_cube_func(cube_func, input_var_names)
-        
-            def cube_func_wrapper(index_chunk, *input_var_chunks):
-                nonlocal input_cube_schema, input_var_names, input_params, input_vars
-                nonlocal has_input_params, has_dim_coords, has_dim_ranges
-        
-                # Note, xarray.apply_ufunc does a test call with empty input arrays,
-                # so index_chunk.size == 0 is a valid case
-                empty_call = index_chunk.size == 0
-        
-                # TODO: when output_var_dims is given, index_chunk must be reordered
-                #   as core dimensions are moved to the and of index_chunk and input_var_chunks
-                if not empty_call:
-                    index_chunk = index_chunk.ravel()
-        
-                if index_chunk.size < 2 * input_cube_schema.ndim:
-                    if not empty_call:
-                        warnings.warn(f"unexpected index_chunk of size {index_chunk.size} received!")
-                        return None
-        
-                dim_ranges = None
-                if has_dim_ranges or has_dim_coords:
-                    dim_ranges = {}
-                    for i in range(input_cube_schema.ndim):
-                        dim_name = input_cube_schema.dims[i]
-                        if not empty_call:
-                            start = int(index_chunk[2 * i + 0])
-                            end = int(index_chunk[2 * i + 1])
-                            dim_ranges[dim_name] = start, end
-                        else:
-                            dim_ranges[dim_name] = ()
-        
-                dim_coords = None
-                if has_dim_coords:
-                    dim_coords = {}
-                    for coord_var_name, coord_var in input_cube_schema.coords.items():
-                        coord_slices = [slice(None)] * coord_var.ndim
-                        for i in range(input_cube_schema.ndim):
-                            dim_name = input_cube_schema.dims[i]
-                            if dim_name in coord_var.dims:
-                                j = coord_var.dims.index(dim_name)
-                                coord_slices[j] = slice(*dim_ranges[dim_name])
-                        dim_coords[coord_var_name] = coord_var[tuple(coord_slices)].values
-        
-                kwargs = {}
-                if has_input_params:
-                    kwargs['input_params'] = input_params
-                if has_dim_ranges:
-                    kwargs['dim_ranges'] = dim_ranges
-                if has_dim_coords:
-                    kwargs['dim_coords'] = dim_coords
-        
-                return cube_func(*input_var_chunks, **kwargs)
-        
-            index_var = _gen_index_var(input_cube_schema)
-        
-            all_input_vars = [index_var] + input_vars
-        
-            input_core_dims = None
-            if output_var_dims:
-                input_core_dims = []
-                has_warned = False
-                for i in range(len(all_input_vars)):
-                    input_var = all_input_vars[i]
-                    var_core_dims = [dim for dim in input_var.dims if dim not in output_var_dims]
-                    must_rechunk = False
-                    if var_core_dims and input_var.chunks:
-                        for var_core_dim in var_core_dims:
-                            dim_index = input_var.dims.index(var_core_dim)
-                            dim_chunk_size = input_var.chunks[dim_index][0]
-                            dim_shape_size = input_var.shape[dim_index]
-                            if dim_chunk_size != dim_shape_size:
-                                must_rechunk = True
-                                break
-                    if must_rechunk:
-                        if not has_warned:
-                            warnings.warn(f'Input variables must not be chunked in dimension(s): {", ".join(var_core_dims)}.\n'
-                                          f'Rechunking applies, which may drastically decrease runtime performance '
-                                          f'and increase memory usage.')
-                            has_warned = True
-                        all_input_vars[i] = input_var.chunk({var_core_dim: -1 for var_core_dim in var_core_dims})
-                    input_core_dims.append(var_core_dims)
-        
-            output_var = xr.apply_ufunc(cube_func_wrapper,
-                                        *all_input_vars,
-                                        dask='parallelized',
-                                        input_core_dims=input_core_dims,
-                                        output_dtypes=[output_var_dtype])
-            if output_var_attrs:
-                output_var.attrs.update(output_var_attrs)
-            return xr.Dataset({output_var_name: output_var}, coords=input_cube_schema.coords)
+        update the cube with the provided Dataset or DataArray.
 
+        Parameters
+        ----------
+        da : Dataset or DataArray
+            should contain the data to be updated to the cube
+        njobs : int
+            number of CPUs to use in parallel when updating,
+            each variable will be updated in parallel
+        initialize : bool
+            set false to skip variable initialization,
+            faster if variables are pre-initialized
+        """
+        
+        if da['latitude'] != self.cube['latitude']:
+            da['latitude'] = self.cube['latitude']
+        
+        if da['longitude'] != self.cube['longitude']:
+            da['longitude'] = self.cube['longitude']
+        
+        if initialize:
+            self.init_variable(da, njobs=njobs)
+            
+        self._update(da, njobs=njobs)
+    
 
-    
-    
-    
