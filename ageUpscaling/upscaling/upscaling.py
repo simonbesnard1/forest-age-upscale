06b4c6f6bdbac9ac356f891f2e926b0ea6230472
Simon <besnard@gfz-potsdam.de>
2022-12-21 15:26:03 +0100

    fix best model dump

--- ageUpscaling/upscaling/upscaling.py
+++ ageUpscaling/upscaling/upscaling.py
@@ -221,8 +221,8 @@ class UpscaleAge(ABC):
         if not os.path.exists(self.study_dir + '/save_model/'):
              os.makedirs(self.study_dir + '/save_model/')
              
-        with open(self.study_dir + "/save_model/best_regressor_run{id_}.pickle".format(id_ = run_), "rb") as fin:
-            mlp_method.best_model = pickle.load(fin)    
+        with open(self.study_dir + "/save_model/best_regressor_run{id_}.pickle".format(id_ = run_), "rb") as fout:
+            pickle.dump(mlp_method.best_model, fout)
             
         return {'best_model': mlp_method.best_model, 'selected_features': mlp_method.final_features, 'norm_stats' : mlp_method.mldata.norm_stats}
     
