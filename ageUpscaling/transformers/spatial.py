3ba8554dd687d972380b79f1f0af246eeb68616c
Simon <besnard@gfz-potsdam.de>
2022-12-22 16:35:08 +0100

    documentation ageUpscaling/transformers/spatial.py

--- ageUpscaling/transformers/spatial.py
+++ ageUpscaling/transformers/spatial.py
@@ -1,14 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec 21 17:54:54 2022
-
-@author: simon
+@author: sbesnard
+@File    :   spatial.py
+@Time    :   Mon Sep 26 10:47:17 2022
+@Author  :   Simon Besnard
+@Version :   1.0
+@Contact :   besnard.sim@gmail.com
+@License :   (C)Copyright 2022-2023, GFZ-Potsdam
+@Desc    :   Methods for spatial operation
 """
+from typing import Union
 
-def interpolate_worlClim(source_ds, 
-                         target_ds,
-                         method:str = 'linear'):
+import xarray as xr
+
+def interpolate_worlClim(source_ds:Union[xr.DataArray, xr.Dataset], 
+                         target_ds:Union[xr.DataArray, xr.Dataset],
+                         method:str = 'linear') -> Union[xr.DataArray, xr.Dataset]:    
+    """
+    Interpolates the source dataset to the coordinates of the target dataset.
+    
+    Parameters
+    ----------
+    source_ds: xr.DataArray or xr.Dataset
+        The source dataset to be interpolated.
+    target_ds: xr.DataArray or xr.Dataset
+        The target dataset with the coordinates to interpolate to.
+    method: str, optional
+        The interpolation method to use. Supported methods are 'linear' (default), 'nearest', 'zero', 'slinear', 'quadratic', 'cubic'.
+        
+    Returns
+    -------
+    resampled: xr.DataArray or xr.Dataset
+        The interpolated source dataset.
+    
+    Raises
+    ------
+    ValueError
+        If the interpolation fails in either the latitude or longitude axis.
+    """
     
     resampled = source_ds.interp(latitude = target_ds.latitude, 
                                  longitude = target_ds.longitude,
