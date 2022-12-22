83fc92869c3022d9d374e3d97bafa7ff7616ff7f
Simon <besnard@gfz-potsdam.de>
2022-12-22 16:23:02 +0100

    documentation ageUpscaling/methods/MLP.py

--- ageUpscaling/methods/MLP.py
+++ ageUpscaling/methods/MLP.py
@@ -10,38 +10,38 @@
 @License :   (C)Copyright 2022-2023, GFZ-Potsdam
 @Desc    :   A method class for training MLP model
 """
-#%% Load library
 import os
-from typing import Any
 import numpy as np
 import pickle
-import optuna
-from sklearn.metrics import mean_squared_error, balanced_accuracy_score
+from typing import Any
+
 import xarray as xr
+
 from sklearn.neural_network import MLPRegressor, MLPClassifier
+from sklearn.metrics import mean_squared_error, balanced_accuracy_score
+
+import optuna
+
 from ageUpscaling.dataloaders.ml_dataloader import MLDataModule
 from ageUpscaling.methods.feature_selection import FeatureSelection
 
 class MLPmethod:
-    """MLPMethod(tune_dir:str=None)
-    A method class for training MLP model
+    """A method class for training and evaluating an MLP model.
     
     Parameters
     ----------
     tune_dir : str, default is None
-    
-        string defining the directory to save the model experiment
-        
-    Return
-    ------
-    a model: the best model of the optuna experiment
+        Directory to save the model experiment. If not provided, the model experiment will not be saved.
+    DataConfig : dict, default is None
+        Dictionary containing the data configuration.
+    method : str, default is 'MLPRegressor'
+        String defining the type of MLP model to use. Can be 'MLPRegressor' for a regression model or 'MLPClassifier' for a classification model.
     """
     
-    def __init__(
-            self,
-            tune_dir: str=None,
-            DataConfig:dict=None,
-            method:str = 'MLPRegressor') -> None:
+    def __init__(self,
+                 tune_dir: str=None,
+                 DataConfig:dict=None,
+                 method:str = 'MLPRegressor') -> None:
 
         self.tune_dir = tune_dir
         
@@ -51,16 +51,39 @@ class MLPmethod:
         self.DataConfig = DataConfig
         self.method = method
         
-    def get_datamodule(
-            self, 
-            method:str = 'MLPRegressor',
-            DataConfig: dict[str, Any] = {},
-            target: dict[str, Any] = {},
-            features: dict[str, Any] = {},
-            train_subset: dict[str, Any] = {},
-            valid_subset: dict[str, Any] = {},
-            test_subset: dict[str, Any] = {},
-            **kwargs) -> dict:
+    def get_datamodule(self, 
+                       method:str = 'MLPRegressor',
+                       DataConfig: dict[str, Any] = {},
+                       target: dict[str, Any] = {},
+                       features: dict[str, Any] = {},
+                       train_subset: dict[str, Any] = {},
+                       valid_subset: dict[str, Any] = {},
+                       test_subset: dict[str, Any] = {},
+                       **kwargs) -> MLDataModule:
+        """Returns the data module for training the model.
+
+        Parameters:
+            method: str, default is 'MLPRegressor'
+                The type of model to use for training ('MLPRegressor' or 'MLPClassifier').
+            DataConfig: dict[str, Any]
+                The data configuration.
+            target: dict[str, Any]
+                The target variables and their configuration.
+            features: dict[str, Any]
+                The features and their configuration.
+            train_subset: dict[str, Any]
+                The subset of the data to use for training.
+            valid_subset: dict[str, Any]
+                The subset of the data to use for validation.
+            test_subset: dict[str, Any]
+                The subset of the data to use for testing.
+            **kwargs:
+                Additional keyword arguments to pass to the `MLDataModule` constructor.
+    
+        Returns:
+            mlData: MLDataModule
+                The data module for training the model.
+        """
         
         mlData = MLDataModule(method,
                               DataConfig,
@@ -79,6 +102,23 @@ class MLPmethod:
               feature_selection:bool= False,
               feature_selection_method:str="recursive", 
               n_jobs:int=10) -> None:
+        
+        """Trains an MLP model using the specified training and validation datasets.
+
+        Parameters:
+            train_subset: dict
+                Dictionary containing the training dataset.
+            valid_subset: dict
+                Dictionary containing the validation dataset.
+            test_subset: dict
+                Dictionary containing the test dataset.
+            feature_selection: bool, optional
+                If True, performs feature selection on the training data before training the model.
+            feature_selection_method: str, optional
+                Method to use for feature selection. Must be one of "boruta" or "recursive".
+            n_jobs: int, optional
+                Number of jobs to use when fitting the model.
+        """
 
         if feature_selection:
             mldata_feature_sel = self.get_datamodule(method= self.method,
@@ -126,7 +166,27 @@ class MLPmethod:
                    val_data:dict,
                    DataConfig:dict,
                    tune_dir:str) -> float:
+        """Searches for the optimal hyperparameters for the machine learning model.
         
+        Parameters
+        ----------
+        trial: optuna.Trial
+            The trial object for the hyperparameter optimization.
+        train_data: dict
+            A dictionary containing the training data.
+        val_data: dict
+            A dictionary containing the validation data.
+        DataConfig: dict
+            A dictionary containing the data configuration.
+        tune_dir: str
+            The directory to save the model experiment.
+            
+        Returns
+        -------
+        float
+            The loss of the model.
+        """
+    
         hyper_params = {
             'learning_rate_init': trial.suggest_float('learning_rate_init ', DataConfig['hyper_params']['learning_rate_init']['min'], DataConfig['hyper_params']['learning_rate_init']['max'], step=DataConfig['hyper_params']['learning_rate_init']['step']),
             'learning_rate': trial.suggest_categorical('learning_rate', DataConfig['hyper_params']['learning_rate']),
@@ -179,9 +239,14 @@ class MLPmethod:
             loss_ =  balanced_accuracy_score(val_data['target'], model_.predict(val_data['features']))
         return loss_ 
     
-    def predict_clusters(
-            self, 
-            save_cube:str) -> xr.Dataset:
+    def predict_clusters(self, 
+                        save_cube:str) -> None:
+        """Predict the target variables for the given data and save the results in a prediction zarr cube.
+    
+        Parameters:
+            save_cube: str
+                Path to the output netCDF file where the predictions will be saved.
+        """
         
         X = self.mldata.test_dataloader().get_x(features= self.final_features)
         Y = self.mldata.test_dataloader().get_y(target= self.DataConfig['target'], 
@@ -200,8 +265,19 @@ class MLPmethod:
                 save_cube.compute_cube(preds, initialize=True, njobs=1)
    
     def denorm_target(self, 
-             x: np.array) -> np.array:
-        """Returns de-normalize target, last dimension of `x` must match len of `self.target_norm_stats`."""
+                      x: np.array) -> np.array:
+        """
+        Returns de-normalized target. 
+        
+        The last dimension of `x` must match the length of `self.target_norm_stats`.
+        
+        Parameters:
+            x: np.array
+                The array to de-normalize.
+        
+        Returns:
+            np.array: The de-normalized array.
+        """
         
         return x * self.mldata.norm_stats[self.DataConfig['target'][0]]['std'] + self.mldata.norm_stats[self.DataConfig['target'][0]]['mean']
                 
