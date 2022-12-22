8112095297ca8c9bc354db8242d9fac0b4c008ec
Simon <besnard@gfz-potsdam.de>
2022-12-22 15:43:51 +0100

    documentation ageUpscaling/dataloaders/ml_dataloader.py

--- ageUpscaling/dataloaders/ml_dataloader.py
+++ ageUpscaling/dataloaders/ml_dataloader.py
@@ -1,26 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: sbesnard
+@File    :   ml_dataloader.py
+@Time    :   Mon Sep 26 10:47:17 2022
+@Author  :   Simon Besnard
+@Version :   1.0
+@Contact :   besnard@gfz-potsdam.de
+@License :   (C)Copyright 2022-2023, GFZ-Potsdam
+@Desc    :   A method class for generating dataloaders
+"""
 from typing import Any
-import numpy as np
+
 import xarray as xr
+
 from ageUpscaling.dataloaders.base import MLData
 
-class MLDataModule:
-    """Define dataloaders.
-
+class MLDataModule(MLData):
     
-    Parameters:
-        cube_path: str
-            Path to the datacube.
-        DataConfig: DataConfig
-            The data configuration.
-        train_subset: Dict[str, Any]:
-            Training set selection.
-        valid_subset: Dict[str, Any]:
-            Same as `train_subset`, for validation set.
-        test_subset: Dict[str, Any]:
-            Same as `train_subset`, for test set.
-        **kwargs:
-            Keyword arguments passed to `DataLoader`, same for training, validation and test set loader.
-    """
     def __init__(
             self,
             method:str='MLPRegressor',
@@ -32,6 +29,23 @@ class MLDataModule:
             test_subset: dict[str, Any] = {},
             norm_stats: dict[str, dict[str, float]] = {},
             **kwargs) -> None:
+        
+        """Define dataloaders.
+
+        Parameters:
+            cube_path: str
+                Path to the datacube.
+            DataConfig: DataConfig
+                The data configuration.
+            train_subset: Dict[str, Any]:
+                Training set selection.
+            valid_subset: Dict[str, Any]:
+                Same as `train_subset`, for validation set.
+            test_subset: Dict[str, Any]:
+                Same as `train_subset`, for test set.
+            **kwargs:
+                Keyword arguments passed to `DataLoader`, same for training, validation and test set loader.
+        """
         
         super().__init__()
 
@@ -53,10 +67,13 @@ class MLDataModule:
                 data_std = data.std().compute().item()
                 self.norm_stats[var] = {'mean': data_mean, 'std': data_std}
 
-    def train_dataloader(self) -> np.array:
-        """Returns the training dataloader."""
+    def train_dataloader(self) -> MLData:
+        """Returns a dataloader for the training set.
 
-
+        Returns:
+            MLData: Dataloader for the training set, containing the features and target values for the training set.
+        """
+        
         train_data = MLData(self.method, 
                             self.DataConfig, 
                             self.target, 
@@ -66,9 +83,13 @@ class MLDataModule:
             
         return train_data
 
-    def val_dataloader(self) -> np.array:
-        """Returns the validation dataloader."""
+    def val_dataloader(self) -> MLData:
+        """Returns a dataloader for the validation set.
 
+        Returns:
+            MLData: Dataloader for the validation set, containing the features and target values for the validation set.
+        """
+        
         valid_data = MLData(self.method, 
                             self.DataConfig, 
                             self.target, 
@@ -78,8 +99,12 @@ class MLDataModule:
             
         return valid_data  
 
-    def test_dataloader(self) -> np.array:
-        """Returns the test dataloader."""
+    def test_dataloader(self) -> MLData:
+        """Returns a dataloader for the test set.
+
+        Returns:
+            MLData: Dataloader for the test set, containing the features and target values for the test set.
+        """
 
         test_data = MLData(self.method, 
                            self.DataConfig,
