b0f59a72c95be55eff6be64071ec2f5af7ddb5f6
Simon <besnard@gfz-potsdam.de>
2023-01-05 14:40:24 +0100

    finish up xgboost module

--- ageUpscaling/methods/__init__.py
+++ ageUpscaling/methods/__init__.py
@@ -1,7 +1,9 @@
 from ageUpscaling.methods.feature_selection import FeatureSelection
 from ageUpscaling.methods.MLP import MLPmethod
+from ageUpscaling.methods.xgboost import XGBoost
 
 __all__ = [
     'FeatureSelection',
-    'MLPmethod'
+    'MLPmethod',
+    'XGBoost'
 ]
