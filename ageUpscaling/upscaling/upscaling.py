ef7db3a9f10263e894cab40157e4ea1cf28c8a8e
Simon <besnard@gfz-potsdam.de>
2023-06-12 13:12:12 +0200

    fix upscaling

--- ageUpscaling/upscaling/upscaling.py
+++ ageUpscaling/upscaling/upscaling.py
@@ -275,11 +275,12 @@ class UpscaleAge(ABC):
                                                           'members': [run_]}, 
                                                   dims=["latitude", "longitude", "time", "members"]))
                             
-            output_reg_xr = xr.concat(output_reg_xr, dim = 'members')
-            output_reg_xr_mean = output_reg_xr.mean(dim = 'members').to_dataset(name="forest_age_mean")
-            output_reg_xr_std = output_reg_xr.std(dim = 'members').to_dataset(name="forest_age_std")
-            self.pred_cube.update_cube(output_reg_xr_mean, initialize=False)
-            self.pred_cube.update_cube(output_reg_xr_std, initialize=False)
+            if len(output_reg_xr>0):
+                output_reg_xr = xr.concat(output_reg_xr, dim = 'members')
+                output_reg_xr_mean = output_reg_xr.mean(dim = 'members').to_dataset(name="forest_age_mean")
+                output_reg_xr_std = output_reg_xr.std(dim = 'members').to_dataset(name="forest_age_std")
+                self.pred_cube.update_cube(output_reg_xr_mean, initialize=False)
+                self.pred_cube.update_cube(output_reg_xr_std, initialize=False)
                     
     def model_tuning(self,
                      run_: int=1,
