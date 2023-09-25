358495e3dd03a06b5b77afffc99dcc30919876df
Simon <besnard@gfz-potsdam.de>
2023-09-25 17:33:24 +0200

    finish age fraction module

--- dev/run_age_class_fraction.py
+++ dev/run_age_class_fraction.py
@@ -11,5 +11,5 @@ from ageUpscaling.upscaling.age_class_fraction import AgeFraction
 #%% Run upscaling
 DataConfig_path= "/home/simon/Documents/science/GFZ/projects/forest-age-upscale/experiments/age_class_fraction/config_age_fraction.yaml"
 calc_age_fraction = AgeFraction(Config_path = DataConfig_path,
-                                study_dir= '/home/simon/gfz_hpc/projects/forest-age-upscale/output/upscaling/Age_upscale_1km/XGBoost/version-1.0/AgeUpscale_1km/')
+                                study_dir= '/home/simon/gfz_hpc/projects/forest-age-upscale/output/upscaling/Age_upscale_1km/XGBoost/version-1.0/')
 calc_age_fraction.AgeFractionCalc()
