4ea97db066326f181553080796ea18cf7a52916c
Simon <besnard@gfz-potsdam.de>
2023-10-30 16:32:24 +0100

    remove autogluon

--- ageUpscaling/methods/__init__.py
+++ ageUpscaling/methods/__init__.py
@@ -2,13 +2,10 @@ from ageUpscaling.methods.feature_selection import FeatureSelection
 from ageUpscaling.methods.MLP import MLPmethod
 from ageUpscaling.methods.xgboost import XGBoost
 from ageUpscaling.methods.RandomForest import RandomForest
-from ageUpscaling.methods.autoML import AutoML
-
 
 __all__ = [
     'FeatureSelection',
     'MLPmethod',
     'XGBoost',
     'RandomForest',
-    'AutoML'
 ]
