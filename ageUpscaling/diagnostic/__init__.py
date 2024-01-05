3f11f8e8f3f5f3e90cf5feeb35f5c40b2664dabf
Simon <besnard@gfz-potsdam.de>
2024-01-05 15:40:35 +0100

    finalize biomass partition module

--- ageUpscaling/diagnostic/__init__.py
+++ ageUpscaling/diagnostic/__init__.py
@@ -1,8 +1,11 @@
 from ageUpscaling.diagnostic.report import Report
 from ageUpscaling.diagnostic.age_partition import DifferenceAge
+from ageUpscaling.diagnostic.biomass_partition import DifferenceBiomass
+
 
 __all__ = [
     'Report',
-    'DifferenceAge'
+    'DifferenceAge',
+    'DifferenceBiomass'
 ]
 
